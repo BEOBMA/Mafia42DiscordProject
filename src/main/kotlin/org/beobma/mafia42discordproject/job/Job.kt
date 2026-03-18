2b196f7d474fe5fd902602b5abb7375177ebf26a
BEOBMA <ssdssdtkfk@naver.com>
2026-03-19 04:14:23 +0900

    feat: upload

--- src/main/kotlin/org/beobma/mafia42discordproject/job/Job.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/Job.kt
@@ -6,5 +6,5 @@ import org.beobma.mafia42discordproject.job.ability.JobUniqueAbility
 abstract class Job {
     abstract val name: String
     abstract val description: String
-    abstract val uniqueAbilities: List<JobUniqueAbility> // 직업 고유 기본능력
+    open val uniqueAbilities: List<JobUniqueAbility> = listOf()
 }
