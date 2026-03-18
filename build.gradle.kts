206f4920eea09948d37add829eb86f4a78cd86fc
BEOBMA <ssdssdtkfk@naver.com>
2026-03-18 22:14:47 +0900

    feat: upload

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "org.beobma"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://snapshots.kord.dev")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("dev.kord:kord-core:0.18.1")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.example
+
+import dev.kord.common.annotation.KordPreview
+
+@KordPreview
+suspend fun main(args: Array<String>) {
+
+
+}
