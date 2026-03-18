83d26447a8c94b5c0f8f749d7b2e7139f3bf6de2
BEOBMA <dingxiao19@gmail.com>
2026-03-19 00:43:38 +0900

    Fix job autocomplete option loading failure

--- src/main/kotlin/org/beobma/mafia42discordproject/command/DiscordCommand.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/command/DiscordCommand.kt
@@ -2,6 +2,7 @@ package org.beobma.mafia42discordproject.command
 
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.kord.core.event.interaction.GuildAutoCompleteInteractionCreateEvent
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
 
 interface DiscordCommand {
@@ -9,6 +10,8 @@ interface DiscordCommand {
     val description: String
 
     suspend fun handle(event: GuildChatInputCommandInteractionCreateEvent)
+
+    suspend fun handleAutoComplete(event: GuildAutoCompleteInteractionCreateEvent) = Unit
 
     suspend fun registerGlobal(kord: Kord) {
         kord.createGlobalChatInputCommand(name, description)
