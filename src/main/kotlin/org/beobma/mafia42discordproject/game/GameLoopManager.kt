8d6ec2c4405b2b43222c2fa3c054866641cb239f
BEOBMA <ssdssdtkfk@naver.com>
2026-03-21 01:36:09 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/mafia42discordproject/game/GameLoopManager.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/game/GameLoopManager.kt
@@ -71,9 +71,6 @@ object GameLoopManager {
     suspend fun startNightPhase(game: Game) {
         game.currentPhase = GamePhase.NIGHT
         game.dayCount++
-        game.mainChannel?.edit {
-            name = "${game.dayCount}일차 밤"
-        }
         game.sendMainChannerImage("https://cdn.discordapp.com/attachments/1483977619258212392/1483978042673070342/43e6c3860a090af9.png?ex=69be8800&is=69bd3680&hm=1dabf5630544f8f8766c7abbb0793a48e3a11e1364a31d1e4e439fff70539e25&")
         game.sendMainChannerMessage("밤이 되었습니다.")
 
@@ -269,9 +266,6 @@ object GameLoopManager {
         game.sendMainChannerMessage(deathMessage)
         delay(3_000L) // 3초 딜레이
 
-        game.mainChannel?.edit {
-            name = "${game.dayCount}일차 낮"
-        }
         game.sendMainChannerImage("https://cdn.discordapp.com/attachments/1483977619258212392/1483981622096429247/7aace941ae58a6cc.png?ex=69bc9115&is=69bb3f95&hm=fc7255667bb001a0f730a3e42d5d729c8584db33095699bcb02fc4ea4295a613&")
         game.sendMainChannerMessage("날이 밝았습니다.")
 
@@ -337,6 +331,7 @@ object GameLoopManager {
 
         val alivePlayers = game.playerDatas.filter { !it.state.isDead }
 
+        game.sendMainChannerImage("https://cdn.discordapp.com/attachments/1483977619258212392/1483981201428709456/bd6d8d833d736bf2.png?ex=69be8af1&is=69bd3971&hm=1d7a852ae35811bd1122ca3b5490bc870c651b0aee66c88bf2fcc9d9e1b3e9e7&")
         mainChannel.createMessage {
             content = "🗳️ **투표 시간이 되었습니다.** 처형할 의심자를 선택해주세요.\n*(시간 내에 선택하지 않으면 무효표 처리됩니다.)*"
 
