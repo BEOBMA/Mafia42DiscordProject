bd88b04de1f061204399d735e158d78bac1d2069
BEOBMA <ssdssdtkfk@naver.com>
2026-03-22 05:55:39 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/mafia42discordproject/job/definition/list/Police.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/definition/list/Police.kt
@@ -7,8 +7,8 @@ import org.beobma.mafia42discordproject.job.ability.general.definition.list.poli
 import org.beobma.mafia42discordproject.job.definition.Definition
 
 class Police : Job(), Definition {
-    override val name: String = "寃쎌같"
-    override val description: String = "[?섏깋] 諛ㅻ쭏???뚮젅?댁뼱 ??紐낆쓣 議곗궗?섏뿬 洹??뚮젅?댁뼱??留덊뵾???щ?瑜??뚯븘?????덈떎."
+    override val name: String = "경찰"
+    override val description: String = "[수색] 밤마다 플레이어 한 명을 조사하여 그 플레이어의 마피아 여부를 알아낼 수 있다."
     override val jobImage: String = "https://media.discordapp.net/attachments/1483977619258212392/1484548811341959208/chrome_WYFnipB5Ox.png?ex=69bea152&is=69bd4fd2&hm=036b6ff6a21169f03b591b9678a8db7ef520aade90b58af8c94e1d557e9083ab&=&format=webp&quality=lossless"
     override val abilities: MutableList<JobUniqueAbility> = mutableListOf(PoliceAbility())
 
