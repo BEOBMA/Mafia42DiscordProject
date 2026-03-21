9485a8564739c83ce5718b2373a057c0b1c886b8
BEOBMA <dingxiao19@gmail.com>
2026-03-22 08:15:21 +0900

    Adjust dawn/day timing and phase messaging

--- src/main/kotlin/org/beobma/mafia42discordproject/game/GamePhase.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/game/GamePhase.kt
@@ -5,6 +5,7 @@ import org.beobma.mafia42discordproject.game.player.PlayerData
 enum class GamePhase {
     DAY,    // 낮
     NIGHT,  // 밤
+    DAWN,   // 밤 -> 낮 전환(새벽)
     VOTE,   // 투표시간
     END     // 끝
 }
