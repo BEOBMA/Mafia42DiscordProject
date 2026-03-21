5be4a5f6546c9addf129271fde712dda07b7c13e
BEOBMA <ssdssdtkfk@naver.com>
2026-03-21 19:45:02 +0900

    feat: 밀서 이미지 수정

--- src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/list/SecretLetter.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/list/SecretLetter.kt
@@ -6,5 +6,5 @@ import org.beobma.mafia42discordproject.job.ability.CommonAbility
 class SecretLetter : Ability, CommonAbility {
     override val name: String = "밀서"
     override val description: String = "게임에서 1회에 한해 받은 사람만이 볼 수 있는 편지를 보낼 수 있다. 편지는 낮이 될 때 도착한다."
-    override val image: String = "https://cdn.discordapp.com/attachments/1483977619258212392/1484601709803343932/36be57cd11ad1686.png?ex=69bed296&is=69bd8116&hm=59c8a6640abee34e17eb92e490536672ec1ddc7c2492417755f94bfe2b0d68a7&"
+    override val image: String = "https://cdn.discordapp.com/attachments/1483977619258212392/1484685904667873380/662ad7bbd34636f1.png?ex=69bf20ff&is=69bdcf7f&hm=c7837766b08ea0dfc2ebe7329ae27f1f463df8ba2d36cdcb3563b9fffab18d67&"
 }
