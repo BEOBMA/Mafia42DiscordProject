36c5acec0a5831b92e4cf1aeba110a34a68946fe
BEOBMA <dingxiao19@gmail.com>
2026-03-22 02:01:25 +0900

    Tighten Arcana target selection to preserve role inference

--- src/main/kotlin/org/beobma/mafia42discordproject/job/definition/list/Fortuneteller.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/definition/list/Fortuneteller.kt
@@ -1,10 +1,16 @@
 package org.beobma.mafia42discordproject.job.definition.list
 
 import org.beobma.mafia42discordproject.job.Job
+import dev.kord.common.entity.Snowflake
+import org.beobma.mafia42discordproject.job.ability.JobUniqueAbility
+import org.beobma.mafia42discordproject.job.ability.general.definition.list.fortuneteller.FortunetellerAbility
 import org.beobma.mafia42discordproject.job.definition.Definition
 
 class Fortuneteller : Job(), Definition {
     override val name: String = "점쟁이"
     override val description: String = "[운세] 밤마다 한 명을 선택한다. 선택한 플레이어 및 그와 다른 팀인 플레이어의 직업이 제시된다."
     override val jobImage: String = "https://media.discordapp.net/attachments/1483977619258212392/1484548930825093130/chrome_14PwFDtOIU.png?ex=69bea16e&is=69bd4fee&hm=f52dfb105e77bee9994d124cf41eb3df72aa4b9d90ee9f04d5c01b67ec8579cf&=&format=webp&quality=lossless"
+    override val abilities: MutableList<JobUniqueAbility> = mutableListOf(FortunetellerAbility())
+
+    var fixedFortuneTargetId: Snowflake? = null
 }
