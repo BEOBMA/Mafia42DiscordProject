d82cd5d2dfb3b607477dd6e7b8c27b0a6d284258
BEOBMA <ssdssdtkfk@naver.com>
2026-03-21 22:21:32 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/definition/list/doctor/DoctorAbility.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/definition/list/doctor/DoctorAbility.kt
@@ -3,16 +3,14 @@ package org.beobma.mafia42discordproject.job.ability.general.definition.list.doc
 import org.beobma.mafia42discordproject.game.Game
 import org.beobma.mafia42discordproject.game.GamePhase
 import org.beobma.mafia42discordproject.game.player.PlayerData
-import org.beobma.mafia42discordproject.game.system.AttackTier
 import org.beobma.mafia42discordproject.game.system.DefenseTier
 import org.beobma.mafia42discordproject.game.system.GameEvent
 import org.beobma.mafia42discordproject.job.ability.AbilityResult
 import org.beobma.mafia42discordproject.job.ability.ActiveAbility
 import org.beobma.mafia42discordproject.job.ability.JobUniqueAbility
 import org.beobma.mafia42discordproject.job.ability.PassiveAbility
-import org.beobma.mafia42discordproject.job.ability.general.evil.list.mafia.WinOrDead
 
-class Heal : ActiveAbility, JobUniqueAbility {
+class DoctorAbility : ActiveAbility, JobUniqueAbility {
     override val name: String = "치료"
     override val description: String = "밤이 되면 플레이어 한 명을 처형으로부터 치료한다."
     override val image: String = "https://cdn.discordapp.com/attachments/1483977619258212392/1484626955612455174/bef253042e3dab28.png?ex=69beea19&is=69bd9899&hm=99b25a7815280b9c866654be45d3ae55eb1a37c4229fc2216f78e4ca341d49ab&"
