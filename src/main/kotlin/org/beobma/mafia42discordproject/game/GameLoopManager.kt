bfa721ca19a0cd16c9546d096e6bbaf349df0801
BEOBMA <ssdssdtkfk@naver.com>
2026-03-25 23:31:29 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/mafia42discordproject/game/GameLoopManager.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/game/GameLoopManager.kt
@@ -721,8 +721,7 @@ object GameLoopManager {
             }
 
             // 2. 메시지 구성
-            val message = "연인의 희생이 발동했습니다. ${originalTarget.member.effectiveName}(${originalRole})의 대가로 ${deadPlayer.member.effectiveName}(${deadRole?.toDisplayName() ?: "미정"})가 대신 사망했습니다.\n" +
-                    "직업 공개: ${originalTarget.member.effectiveName} - ${originalJobName}, ${deadPlayer.member.effectiveName} - ${deadJobName}"
+            val message = "${deadPlayer.member.effectiveName}님이 연인 ${originalTarget.member.effectiveName}을 살리고 대신 마피아에게 살해 당했습니다!"
 
             // 3. 텍스트 대신 이미지와 함께 전송
             game.sendMainChannelMessageWithImageAndSound(
