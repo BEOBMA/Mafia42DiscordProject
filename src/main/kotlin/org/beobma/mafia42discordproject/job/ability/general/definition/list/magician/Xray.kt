c1e5531298e60e6e0520b46ba2734b539633779e
BEOBMA <ssdssdtkfk@naver.com>
2026-07-17 02:45:02 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/definition/list/magician/Xray.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/definition/list/magician/Xray.kt
@@ -10,5 +10,5 @@ class Xray : Ability, JobSpecificExtraAbility {
     override val name: String = "투시"
     override val description: String = "'트릭' 능력이 발동하고 바꿔치기에 성공한 경우, 그 사람이 누구를 투표했는지 알아낸다."
     override val image: String = "https://lsvptosgnbwgsteuwstf.supabase.co/storage/v1/object/public/mafia/magician_ability_xray_pix_1.webp"
-    override val targetJob: List<KClass<out Job>> = listOf(Magician::class)
+    override val targetJob: List<KClass<out Job>> = listOf()
 }
--- src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/list/Jury.kt
+++ src/main/kotlin/org/beobma/mafia42discordproject/job/ability/general/list/Jury.kt
@@ -5,6 +5,6 @@ import org.beobma.mafia42discordproject.job.ability.CommonAbility
 
 class Jury : Ability, CommonAbility {
     override val name: String = "배심원"
-    override val description: String = "최다 득표자가 생존자의 절반 미만의 득표를 한 상태에서 여러명일 경우, 그 중 배심원이 더 많이 투표한 플레이어가 최후의 반론에 올라간다."
-    override val image: String = "https://lsvptosgnbwgsteuwstf.supabase.co/storage/v1/object/public/mafia/mafia%20(100).webp"
+    override val description: String = "최다 득표자가 생존자의 절반 미만의 득표를 한 상태에서 여러명일 경우, 그 중 배심원이 더 많이 투표한 플레이어가 최후의 반론에 올라간다. 다른 플레이어의 최후의 반론 시간에 말을 할 수 있다."
+    override val image: String = "https://lsvptosgnbwgsteuwstf.supabase.co/storage/v1/object/public/mafia/jury.webp"
 }
